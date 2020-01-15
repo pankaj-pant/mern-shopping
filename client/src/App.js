1c573c22f172035a196289c79a1c38ca92ed4a3b
pankaj-pant <pankajpant89@gmail.com>
2020-01-15 22:11:32 +0200

    added Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import AppNavbar from './components/AppNavbar'
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
+      <AppNavbar />
      <h1>Hello</h1>
     </div>
   );
