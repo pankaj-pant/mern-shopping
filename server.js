e52e38c631429660ac0af5bd23b1eb9661ac5d49
pankaj-pant <pankajpant89@gmail.com>
2020-01-21 15:11:14 +0200

    ready for deployment

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require ('mongoose')
 const bodyParser = require('body-parser')
 
 const items = require('./routes/api/items')
+const path = require('path')
 
 const app = express()
 
@@ -20,6 +21,16 @@ mongoose
 
 //Use routes
 app.use('/api/items', items)
+
+// Serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+    //Set static folder
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 const port = process.env.PORT || 5000
 
